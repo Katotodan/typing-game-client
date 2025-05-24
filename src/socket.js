87d9702aeb0500c7117790b3752d40ee4ba8ee33
unknown <katotodan@gmail.com>
2025-05-24 15:36:43 +0300

    Setting sockect

--- src/socket.js
+++ src/socket.js
@@ -4,5 +4,6 @@ import { io } from 'socket.io-client';
 const URL = process.env.REACT_APP_API_URL
 
 export const socket = io(URL, {
-    autoConnect: false
+  autoConnect: false,
+  transports: ["websocket"]
 }); 
